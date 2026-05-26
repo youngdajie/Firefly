2b333ec9c150128cf9ab9f453d83c92cb78a04f2
youngdajie <ok@yangjie.site>
2026-05-26 19:25:57 +0800

    fix bug

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -128,15 +128,15 @@ export const siteConfig: SiteConfig = {
 	// bangumi的数据为编译时获取的，所以不是实时数据，请配置bangumi.userId
 	pages: {
 		// 友链页面开关
-		friends: false,
+		friends: true,
 		// 赞助页面开关
 		sponsor: true,
 		// 留言板页面开关，需要配置评论系统
-		guestbook: false,
+		guestbook: true,
 		// 番组计划页面开关，含追番、游戏、书籍和音乐，dev调试时只获取一页数据，build才会获取全部数据
 		bangumi: true,
 		// 相册页面开关
-		gallery: false,
+		gallery: true,
 	},
 
 	// 分类导航栏开关，在首页和归档页顶部显示分类快捷导航
