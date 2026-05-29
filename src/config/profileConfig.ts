ec63c356627627bff963b6f901d4f71e910013bc
youngdajie <ok@yangjie.site>
2026-05-30 00:02:29 +0800

    细节修改

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -24,19 +24,19 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "qq",
 			icon: "fa7-brands:qq",
-			url: "https://qm.qq.com/q/ZGsFa8qX2G",
+			url: "tencent://AddContact/?fromId=45&fromSubId=1&subcmd=all&uin=434871910&website=www.qq.com",
 			showName: false,
 		},
 		{
 			name: "GitHub",
 			icon: "fa7-brands:github",
-			url: "https://github.com/CuteLeaf",
+			url: "https://github.com/youngdajie",
 			showName: false,
 		},
 		{
 			name: "Email",
 			icon: "fa7-solid:envelope",
-			url: "mailto:xiaye@msn.com",
+			url: "mailto:ok@yangjie.site",
 			showName: false,
 		},
 		{
